b615d9636b8b9c7c03633d17670ffac623c13145
altaf <altaf98khan66@gmail.com>
2023-08-03 15:29:02 +0530

    Add: Ability to create a Contacts in Address Book Program.

---
+++ src/com/bridgelabz/addressbook292/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.addressbook292;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Address Book System Program.");
+    }
+}
---
+++ src/com/bridgelabz/addressbook292/AddressBookSystem.java
@@ -0,0 +1,4 @@
+package com.bridgelabz.addressbook292;
+
+public class AddressBookSystem {
+}
